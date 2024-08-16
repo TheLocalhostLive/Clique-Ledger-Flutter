36ad656f0a5a561f813cad30e616a6e35a310950
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2024-08-17 04:18:26 +0530

    socket-io: Implement socket-io workflow.
    Currenly if message is sent in a clique it is notified to all other
    users. In this commit we are only handling creation of a transaction.
    Later on other events will be handled.

---
+++ cliqueledger/lib/service/socket_service.dart
@@ -0,0 +1,69 @@
+import 'dart:convert';
+
+import 'package:cliqueledger/models/transaction.dart';
+import 'package:cliqueledger/providers/CliqueListProvider.dart';
+import 'package:cliqueledger/providers/TransactionProvider.dart';
+import 'package:cliqueledger/service/socket_event_handler.dart';
+import 'package:flutter/material.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+
+class SocketService {
+  static final SocketService instance = SocketService._internal();
+
+  IO.Socket? socket;
+  static TransactionProvider? transactionProvider;
+  static CliqueListProvider? cliquesProvider;
+
+  final Map<String, dynamic> _eventHandlerMap = {
+    "transaction-created": (data) => SocketEventHandler.handleCreateTranscation(data, transactionProvider),
+    "transaction-deleted": SocketEventHandler.handleDeleteTransaction,
+    "transaction-accepted": SocketEventHandler.handleAcceptTransaction,
+    "transaction-rejected": SocketEventHandler.handleRejectTransaction,
+    "added-to-clique": SocketEventHandler.handleAddToClique,
+    "removed-from-clique": SocketEventHandler.handleRemoveFromClique,
+  };
+
+  SocketService._internal();
+
+  Transaction? transaction;
+
+  Function(dynamic data)? onLoginEvent;
+  Function(dynamic data)? onLogoutEvent;
+
+  factory SocketService() {
+    return instance;
+  }
+
+  void connectAndListen() {
+    if (socket != null) return;
+    debugPrint("Connection to socket...");
+    socket ??= IO.io('http://13.234.48.56:3000/',
+        IO.OptionBuilder().setTransports(['websocket']).build());
+
+    socket!.onConnect((data) => {print("Connected to socket")});
+
+    socket!.on('session-expired', (data) => {onLogoutEvent?.call(data)});
+
+    socket!.on('session-join', (data) => {onLogoutEvent?.call(data)});
+  }
+
+  //rooms are basically cliqueId
+  void joinRooms(List<String> rooms) {
+    print(rooms);
+    socket!.emit('join-rooms', jsonEncode(rooms));
+  }
+
+  void setupListeners() {
+    _eventHandlerMap.keys.toList().forEach((event) {
+      socket!.on(event, (data) {
+        _eventHandlerMap[event](data);
+      });
+    });
+  }
+
+  void dispose() {
+    if (socket != null) {
+      socket!.disconnect();
+    }
+  }
+}
