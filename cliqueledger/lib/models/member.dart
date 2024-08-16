4c0a605311a9dc67a4c89cdaef77f0d85c789f65
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2024-08-17 04:12:14 +0530

    Member Schema: Add user_id.

--- cliqueledger/lib/models/member.dart
+++ cliqueledger/lib/models/member.dart
@@ -7,9 +7,13 @@ class Member{
   @JsonKey(name: "member_id")
   final String memberId;
 
+  @JsonKey(name: "user_id")
+  final String userId;
+
   Member({
     required this.name,
     required this.memberId,
+    required this.userId
   });
  
   
@@ -18,14 +22,14 @@ class Member{
     return Member(
       name: json["member_name"],
       memberId: json["member_id"],
-    
+      userId: json['user_id']
       );
   }
   Map<String,dynamic> toJson(){
     return {
       'member_name':name,
       'member_id':memberId,
-     
+      'user_id': userId
     };
 
   }
